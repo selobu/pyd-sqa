34ffd5427d1726464a9d652db6c1280be5f5e9bd
Sebastian Lopez Buritica <selobu@gmail.com>
2023-10-02 18:15:01 -0500

    Isort removed lint.sh
    Isort removed from lint

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -5,4 +5,3 @@ set -x
 
 mypy pyd_sqa
 black pyd_sqa tests --check
-isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --check-only --thirdparty pyd_sqa pyd_sqa tests
